83598e15ad183697606f60dc12c6f62ba4171525
danicuello13 <142356422+danicuello13@users.noreply.github.com>
2023-09-08 20:21:05 -0300

    Add files via upload
    recuperatorio resuelto

--- index.js
+++ index.js
@@ -3,7 +3,11 @@
   Implementa la función llamada `sumarNumeros` que tome dos números como argumento y devuelva la suma de esos dos números.
 */
 
-function sumarNumeros() {}
+function sumarNumeros(num) {
+  return num +4
+}
+sumarNumeros(5);
+
 
 /* Actividad 2: Calcular el Factorial de un Número -- 3 PUNTOS
 
@@ -11,15 +15,31 @@ function sumarNumeros() {}
   (recuerden que podemos realizar acciones repetitivas usando un FOR)
 */
 
-function calcularFactorial() {}
-
+function calcularFactorial(num) {
+  if (num == 0)
+    return 1;
+  for (let i = num - 1; i >= 1; i--) {
+    num *= i;
+  }
+  return num;
+}
+calcularFactorial(5);
 /* Actividad 3: Encontrar el Número Mayor en una Matriz -- 5 PUNTOS
 
   Escribe una función llamada `encontrarNumeroMayor` que tome una matriz de números como argumento y devuelva el número mayor de la matriz.
 */
 
-function encontrarNumeroMayor() {}
-
+function encontrarNumeroMayor(arr){
+  let min = arr[0]
+  let max = arr[0]
+  for(let i = 0; i<arr.length; i++){
+    if(min > arr[i]){
+      min = arr[i]
+    }
+  } 
+return [min, max]
+}
+encontrarNumeroMayor([10,7,3,9]);
 
 
 
